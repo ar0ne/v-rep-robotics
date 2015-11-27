86a0564b51778989623dfd1c159ead42d3c80726
ar0ne <camtbitakou@yandex.ru>
2015-11-28 02:45:11 +0300

    added base functions for robot like rotate to zero and target

--- bugs_algorithms/src/scripts/robot.py
+++ bugs_algorithms/src/scripts/robot.py
@@ -17,6 +17,8 @@ class Robot:
         self._hostname = host
         self._port = port
 
+        self._sleep_time = 0.05
+
         self._client_id = self._init_client_id()
         self._left_wheel_handle, self._right_wheel_handle = self._init_wheels_handles()
         self._robot_handle = self._init_robot_handle()
@@ -24,13 +26,14 @@ class Robot:
         self._target_handle = self._init_target_handle()
         self._target_position = self._init_target_position()
         self._robot_orientation = self._init_robot_orientation()
+        self._robot_position = self._init_robot_position()
 
         # orientation of all the sensors:
         self._sensor_loc = np.array([-PI/2, -50/180.0 * PI, -30/180.0 * PI, -10/180.0 * PI, 10/180.0 * PI,
                                      30/180.0 * PI, 50/180.0 * PI, PI/2, PI/2, 130/180.0 * PI, 150/180.0 * PI,
                                      170/180.0 * PI, -170/180.0 * PI, -150/180.0 * PI, -130/180.0 * PI, -PI/2])
 
-        self._sleep_time = 0.2
+
 
     def _init_client_id(self):
 
@@ -59,6 +62,8 @@ class Robot:
     def move(self, v_l = 1, v_r = 1):
         error_code = vrep.simxSetJointTargetVelocity(self._client_id, self._left_wheel_handle,  v_l, vrep.simx_opmode_streaming)
         error_code = vrep.simxSetJointTargetVelocity(self._client_id, self._right_wheel_handle, v_r, vrep.simx_opmode_streaming)
+        self.sleep(0.01)
+        self.stop_move()
 
     def step(self):
         #
@@ -68,45 +73,161 @@ class Robot:
         return 1,1
 
     def _init_target_position(self):
-        error_code, target_position = vrep.simxGetObjectPosition(self._client_id, self._target_handle, self._robot_handle, vrep.simx_opmode_streaming)
+        self.sleep()
+        error_code, target_position = vrep.simxGetObjectPosition(self._client_id, self._target_handle, -1, vrep.simx_opmode_streaming)
         return target_position
 
+    def read_target_position(self):
+        self.sleep()
+        error_code, target_position = vrep.simxGetObjectPosition(self._client_id, self._target_handle, -1, vrep.simx_opmode_buffer)
+        return target_position
+
+    def _init_robot_position(self):
+        self.sleep()
+        error_code, robot_position = vrep.simxGetObjectPosition(self._client_id, self._robot_handle, -1, vrep.simx_opmode_streaming)
+        return robot_position
+
     def _init_robot_orientation(self):
+        self.sleep()
         error_code, euler_angles = vrep.simxGetObjectOrientation(self._client_id, self._robot_handle, -1, vrep.simx_opmode_streaming)
         return euler_angles
 
-    def read_target_position(self):
-        error_code, target_position = vrep.simxGetObjectPosition(self._client_id, self._target_handle, self._robot_handle, vrep.simx_opmode_buffer)
-        return target_position
-
     def read_robot_orientation(self):
+        self.sleep()
         error_code, euler_angles = vrep.simxGetObjectOrientation(self._client_id, self._robot_handle, -1, vrep.simx_opmode_buffer)
         return euler_angles
 
-    def _rotate_robot_to_target(self):
+    def read_robot_position(self):
+        self.sleep()
+        error_code, robot_position = vrep.simxGetObjectPosition(self._client_id, self._robot_handle, -1, vrep.simx_opmode_buffer)
+        return robot_position
 
-        angles = self.read_robot_orientation()
+    def _rotate_to_zero(self):
 
-        norm = np.linalg.norm(angles)
+        print("Rotate to Zero degree...")
 
-        while(norm > 0.02):
+        self.sleep()
+
+        rot1, rot2 = 0, 0
+
+        min_speed = 0.1
+        delta = 0.005
+
+        ornt = self.read_robot_orientation()
+        norm = np.linalg.norm(ornt)
+
+        if ornt[0] > 0:
+            rot1, rot2 = min_speed, -min_speed  # поворачивать против часовой
+        else:
+            rot1, rot2 = -min_speed, min_speed
+
+        while( delta < norm or norm > delta):
 
             angles = self.read_robot_orientation()
 
             norm = np.linalg.norm(angles)
 
-            self.move(0.05, -0.05)
-
-            print(norm)
-
-            self.sleep(0.01)
+            self.move(rot1, rot2)
 
         self.stop_move()
+
+    def _rotate_at_90_degrees(self):
+
+        print("Rotate at 90 degrees...")
+
+        foo = self.read_robot_orientation()
+        ornt = np.linalg.norm(foo)
+        norm = ornt
+        rad_90 = self.deg2rad(90)
+        rad_180 = self.deg2rad(180)
+
+        if foo[0] > 0:
+            if ornt > rad_90:
+                tmp = rad_180 - ornt
+                ornt = rad_90 - tmp
+            else:
+                ornt = rad_90 + ornt
+        else:
+            if ornt < rad_90:
+                ornt = rad_90 - ornt
+            else:
+                tmp = rad_180 - ornt
+                ornt = rad_180 - (rad_90 - tmp)
+
+        desp = 0.005
+        speed = 0.02
+        min = ornt - desp
+        max = ornt + desp
+        while not (norm > min and norm < max):
+            self.move(-speed, speed)
+            norm = np.linalg.norm(self.read_robot_orientation())
+            # print(norm, ornt)
+
+
+    def rotate_robot_to_target(self):
+
+        print("Rotate to target...")
+
+        self._rotate_to_zero()
+
+        alpha = self._get_angles_between_robot_and_target()
+
+        print("ALPHA: " + str(alpha))
+
+        sign = True  # right side
+
+        rad_alpha = 0
+
+        if alpha < 180:
+            sign = True
+            rad_alpha = self.deg2rad(alpha)
+        elif alpha >= 180:
+            sign = False
+            rad_alpha = self.deg2rad(alpha - 180)
+
+        norm = 0
+        desp = 0.005
+        speed = 0.02
+        min = rad_alpha - desp
+        max = rad_alpha + desp
+
+        while not (norm > min and norm < max):
+            if sign:
+                self.move(speed, -speed)
+            else:
+                self.move(-speed, speed)
+                
+            norm = np.linalg.norm(self.read_robot_orientation())
+            # print(norm, rad_alpha)
+
+    def _get_angles_between_robot_and_target(self):
+
+        self.sleep()
+
+        x0, y0, _ = self.read_robot_position()
+        x1, y1, _ = self.read_target_position()
+
+        alpha = math.atan2(y0 - y1, x0 - x1) / PI * 180
+
+        if alpha < 0:
+            alpha += 360
+
+        return alpha
+
+        # if b
+
+    def rad2deg(self, rad):
+        return rad * 180 / PI
+
+    def deg2rad(self, deg):
+        return deg * PI / 180
 
     def read_data_from_face_sensors(self):
         #
         # Read data from first 8 sensors
         #
+        sensor_val = np.array([])
+
         for x in xrange(1, 8 + 1):
             error_code, detection_state, detected_point, detected_object_handle, detected_surface_normal_vector = vrep.simxReadProximitySensor(self._client_id, self._sensors_handle[x], vrep.simx_opmode_buffer)
             sensor_val = np.append(sensor_val, np.linalg.norm(detected_point))
@@ -145,8 +266,8 @@ class Robot:
         return sensor_h, sensor_val
 
     def stop_move(self):
-        errorCode = vrep.simxSetJointTargetVelocity(self._client_id, self._left_wheel_handle,  0, vrep.simx_opmode_streaming)
-        errorCode = vrep.simxSetJointTargetVelocity(self._client_id, self._right_wheel_handle, 0, vrep.simx_opmode_streaming)
+        error_code = vrep.simxSetJointTargetVelocity(self._client_id, self._left_wheel_handle,  0, vrep.simx_opmode_streaming)
+        error_code = vrep.simxSetJointTargetVelocity(self._client_id, self._right_wheel_handle, 0, vrep.simx_opmode_streaming)
 
     def sleep(self, sec=None):
         if sec is None:
@@ -164,12 +285,14 @@ if __name__ == '__main__':
 
     robot = Robot()
 
-    robot.sleep()
+    robot.stop_move()
+
+    robot.rotate_robot_to_target()
+
+    # robot._rotate_at_90_degrees()
 
     robot.stop_move()
 
-    robot._rotate_robot_to_target()
-
-    robot.stop_move()
+    print("END")
 
     # robot.update()
