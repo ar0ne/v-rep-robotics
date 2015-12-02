48cbdba6ce239286db5271a401e41a621a4f9026
ar0ne <camtbitakou@yandex.ru>
2015-12-02 23:38:48 +0300

    refactor

--- bugs_algorithms/src/scripts/bug2_2.py
+++ bugs_algorithms/src/scripts/bug2_2.py
@@ -59,64 +59,15 @@ class Bug2:
             print 'Connection not successful'
             sys.exit('Could not connect')
 
-    def angle_between_vectors(self, a, b):  # a -> b
+    @staticmethod
+    def angle_between_vectors(a, b):  # a -> b
 
-        def unit_vector(vector):
-            n = 1.0 / math.sqrt(np.sum(np.array(vector) ** 2))
-            return vector * n
-
-        a = unit_vector(a)
-        b = unit_vector(b)
-
-        if len(a) == 4:
-            a = a[1:]
-
-        if len(b) == 4:
-            b = b[1:]
-
+        a = a.unitVector()
+        b = b.unitVector()
         angle = math.acos(b.dot(a))
-
-        if (a * b)[2] > 0.0:
+        if (a.multiply(b)).z > 0.0:
             return -angle
         return angle
-
-    def create_quaternion(self, angle, vector):
-        halfAngle = angle / 2.0
-        sinHalfAngle = math.sin(halfAngle)
-        w = math.cos(halfAngle)
-        x = sinHalfAngle * vector[0]
-        y = sinHalfAngle * vector[1]
-        z = sinHalfAngle * vector[2]
-        return np.array([w, x, y, z])
-
-    def rotate(self, q_rot, vector):
-        vect_quad = np.array([0, vector[0], vector[1], vector[2]])
-        q1 = self.multiply_quternions(q_rot, vect_quad)
-        res = self.multiply_quternions(q1, self.inverse_quaternion(q_rot))
-        return res
-
-    def multiply_quternions(self, a, b):
-        w = a[0] * b[0] - a[1] * b[1] - a[2] * b[2] - a[3] * b[3]
-        x = a[0] * b[1] + a[1] * b[0] + a[2] * b[3] - a[3] * b[2]
-        y = a[0] * b[2] - a[1] * b[3] + a[2] * b[0] + a[3] * b[1]
-        z = a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0]
-        return np.array([w, x, y, z])
-
-    def inverse_quaternion(self, q_rot):
-
-        n = self.norm_quaternion(q_rot)
-
-        w =   q_rot[0] / n
-        x = - q_rot[1] / n
-        y = - q_rot[2] / n
-        z = - q_rot[3] / n
-        return np.array([w, x, y, z])
-
-    def norm_quaternion(self, q):
-        return np.sum(np.array(q) ** 2)
-
-    def dot_quaternion(self, q1, q2):
-        return q1[0] * q2[0] + q1[1] * q2[1] + q1[2] * q2[2] + q1[3] * q2[3]
 
     def _init_handles(self):
 
@@ -212,24 +163,18 @@ class Bug2:
             self.stop_move()
             self.read_values()
 
-            # self.targetPos = Vector3(x=self.target_pos[0], y=self.target_pos[1], z=self.target_pos[2])
-            self.targetPos = np.array(self.target_pos)
+            self.targetPos = Vector3(x=self.target_pos[0], y=self.target_pos[1], z=self.target_pos[2])
 
-            # self.botPos = Vector3(x=self.bot_pos[0], y=self.bot_pos[1], z=self.bot_pos[2])
-            self.botPos = np.array(self.bot_pos)
+            self.botPos = Vector3(x=self.bot_pos[0], y=self.bot_pos[1], z=self.bot_pos[2])
 
-            # self.botEulerAngles = Vector3(x=self.bot_euler_angles[0], y=self.bot_euler_angles[1], z=self.bot_euler_angles[2])
-            self.botEulerAngles = np.array(self.bot_euler_angles)
+            self.botEulerAngles = Vector3(x=self.bot_euler_angles[0], y=self.bot_euler_angles[1], z=self.bot_euler_angles[2])
 
             self.read_from_sensors()
 
-            self.targetPos[2] = self.botPos[2] = 0.0
-            # qRot = Quaternion()
-            # qRot.set_from_vector(self.botEulerAngles[2], Vector3( 0.0, 0.0, 1.0 ))
-            # self.botDir = qRot.rotate( Vector3( 1.0, 0.0, 0.0 ) )
-
-            qRot = self.create_quaternion(self.botEulerAngles[2], np.array([0.0, 0.0, 1.0]))
-            self.botDir = self.rotate(qRot, np.array([1.0, 0.0, 0.0]))
+            self.targetPos.z = self.botPos.z = 0.0
+            qRot = Quaternion()
+            qRot.set_from_vector(self.botEulerAngles.z, Vector3(0.0, 0.0, 1.0))
+            self.botDir = qRot.rotate( Vector3(1.0, 0.0, 0.0))
 
             if self.state == State.MOVING:
                 self.action_moving()
@@ -243,16 +188,13 @@ class Bug2:
         if self.detect[4] < 0.6:
 
             self.state = State.ROTATING
-            # tmp = Quaternion()
-            # tmp.set_from_vector(PI / 2.0, Vector3( 0.0, 0.0, 1.0 ))
-            # self.targetDir = tmp.rotate(self.botDir)
-
-            qRot = self.create_quaternion(PI / 2, np.array([0.0, 0.0, 1.0]))
-            self.targetDir = self.rotate(qRot, self.botDir)
+            tmp = Quaternion()
+            tmp.set_from_vector(PI / 2.0, Vector3( 0.0, 0.0, 1.0 ))
+            self.targetDir = tmp.rotate(self.botDir)
 
             return
 
-        angle = self.angle_between_vectors(self.botDir, self.targetPos - self.botPos)
+        angle = self.angle_between_vectors(self.botDir, self.targetPos.minus(self.botPos))
 
         if math.fabs(angle) > 1.0 / 180.0 * PI:
             vrep.simxSetJointTargetVelocity(self.client_id, self.left_motor_handle, self.WHEEL_SPEED + angle, vrep.simx_opmode_streaming)
@@ -272,14 +214,11 @@ class Bug2:
             self.state = State.ENVELOPING
 
     def action_enveloping(self):
-        # tmp_dir = Quaternion()
-        # tmp_dir.set_from_vector(PI / 2.0, Vector3( 0.0, 0.0, 1.0 ))
-        # perp_bot_dir = tmp_dir.rotate(self.botDir)
+        tmp_dir = Quaternion()
+        tmp_dir.set_from_vector(PI / 2.0, Vector3(0.0, 0.0, 1.0))
+        perp_bot_dir = tmp_dir.rotate(self.botDir)
 
-        qRot = self.create_quaternion(PI / 2, np.array([0.0, 0.0, 1.0]))
-        perp_bot_dir = self.rotate(qRot, self.botDir)
-
-        angle = self.angle_between_vectors(perp_bot_dir, self.targetPos - self.botPos)
+        angle = self.angle_between_vectors(perp_bot_dir, self.targetPos.minus(self.botPos))
 
         if math.fabs(angle) < 5.0 / 180.0 * PI:
             self.state = State.MOVING
